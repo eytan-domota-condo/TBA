f04483e7890170b97453cd5a4d7bc2ec2d333249
bijout <thomas.bijou@edu.esiee.fr>
2026-01-05 14:20:06 -0400

    Create item.py

---
+++ item.py
@@ -0,0 +1,10 @@
+# item.py
+
+class Item:
+    def __init__(self, name: str, description: str, weight: int | float):
+        self.name = name
+        self.description = description
+        self.weight = weight
+
+    def __str__(self) -> str:
+        return f"{self.name} : {self.description} ({self.weight} kg)"
